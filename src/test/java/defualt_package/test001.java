0548743aa26b111bb9ddaed73c3ede5a42a81043
SDETRepo001 <hamidiabdul6@gmail.com>
2020-10-25 17:44:41 -0400

    First add

--- src/test/java/defualt_package/test001.java
+++ src/test/java/defualt_package/test001.java
@@ -16,7 +16,7 @@ public class test001 {
 	
 	@Test
 	void test0001 () {
-		
+		// baryali
 		Response response =get("https://reqres.in/api/users?page=2");
 		
 		System.out.println(response.asString());
