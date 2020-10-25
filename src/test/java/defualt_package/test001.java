2681db05e87087bbe9e60dc09349b388453d9410
SDETRepo001 <hamidiabdul6@gmail.com>
2020-10-25 17:24:57 -0400

    first commit

---
+++ src/test/java/defualt_package/test001.java
@@ -0,0 +1,51 @@
+package defualt_package;
+
+import static org.testng.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class test001 {
+
+	
+	
+	@Test
+	void test0001 () {
+		
+		Response response =get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		System.out.println(response.getTime());
+		
+		int statuscode = response.statusCode();
+
+		Assert.assertEquals(statuscode, 200);
+		
+		
+		System.out.println(statuscode);
+		
+		
+	}
+	
+	
+	void Test_002 () {
+		
+		given()
+		.get("https://reqres.in/api/users?page=2")
+		.then()
+		.statusCode(201).body("data.id[0]", equalTo (7));
+		
+		
+	
+	
+	}
+}
