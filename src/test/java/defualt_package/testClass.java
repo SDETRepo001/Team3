ec35c4716536e47df5b13364a0227f7fed6f1948
sharafuddinb <sharafuddin.baryali@gmail.com>
2020-10-28 23:45:29 -0400

    from eclipse IDE

--- src/test/java/defualt_package/testClass.java
+++ src/test/java/defualt_package/testClass.java
@@ -4,5 +4,9 @@ public class testClass {
 	
 	//Jamshed Ghair Hazer hast
 	// Az group bayad delet shawa
+	
+	public static void main(String[] args) {
+		System.out.println("Where is Jamshed");
+	}
 
 }
