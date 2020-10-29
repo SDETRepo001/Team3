1c4ef0c61815cb7b39ea2994a57460c983767771
SDETRepo001 <hamidiabdul6@gmail.com>
2020-10-28 23:54:46 -0400

    Jamshid is bad

---
+++ src/test/java/defualt_package/Jamshed.java
@@ -0,0 +1,20 @@
+package defualt_package;
+
+public class Jamshed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =10;
+		int b =20;
+		
+		if (a > b )
+		{
+			System.out.println("Jamshid is not good");
+		}
+		else {
+			System.out.println("Jamshid is worse");
+		}
+	}
+
+}
